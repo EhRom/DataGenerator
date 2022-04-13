96b35e6c5acdc7be0236b31c28fea018631b84ee
Romain EHRET <ehrom@outlook.com>
2022-04-14 00:07:46 +0200

    Add project files.

---
+++ DataGenerator/Domain/GeneratorService.cs
@@ -0,0 +1,124 @@
+﻿using Puffix.ConsoleLogMagnifier;
+
+namespace DataGenerator.Domain
+{
+    public class GeneratorService
+    {
+        public GeneratorService()
+        { }
+
+        public static GeneratorService CreateNew()
+        {
+            return new GeneratorService();
+        }
+
+        public void SetStartAndEndDate(int maxTryCount, out DateOnly startDate, out DateOnly endDate)
+        {
+            int currentTryCount;
+            currentTryCount = 0;
+            while (!EnterDate(maxTryCount, ++currentTryCount, "start", out startDate)) { }
+
+            currentTryCount = 0;
+            while (!EnterDate(maxTryCount, ++currentTryCount, "end", out endDate)) { }
+
+            if(startDate > endDate)
+            {
+                DateOnly tempDate = startDate;
+                startDate = endDate;
+                endDate = tempDate;
+            }
+        }
+
+        public bool EnterDate(int maxTryCount, int currentTryCount, string dateName, out DateOnly date)
+        {
+            ConsoleHelper.WriteInfo($"Enter the {dateName} date (dd/MM/yyyy or yyyy-MM-dd formats, years between 2000 and 2100):");
+            if (currentTryCount > 1)
+                ConsoleHelper.WriteVerbose($"Try {currentTryCount} / {maxTryCount}");
+            string? enteredDate = Console.ReadLine();
+
+            ConsoleHelper.ClearLastLines();
+            
+            bool valid = !string.IsNullOrEmpty(enteredDate) && ValidateDate(enteredDate, out date);
+
+            if (!valid)
+            {
+                ConsoleHelper.WriteWarning($"The entered date ('{enteredDate}') is not valid.");
+                if (currentTryCount >= maxTryCount)
+                    throw new ArgumentException("The entered date is not in the right format. Format accepted: dd/MM/yyyy or yyyy-MM-dd, years between 2000 and 2100.");
+            }
+            else
+            {
+                ConsoleHelper.WriteSuccess($"The {dateName} date is '{enteredDate}'.");
+            }
+
+            return valid;
+        }
+
+        public bool ValidateDate(string dateValue, out DateOnly date)
+        {
+            const string isoDateFormat = "yyyy-MM-dd";
+            const string frenchDateFormat = "dd/MM/yyyy";
+
+            bool isValid = DateOnly.TryParseExact(dateValue, isoDateFormat, out date) ||
+                           DateOnly.TryParseExact(dateValue, frenchDateFormat, out date);
+
+            isValid = isValid && date.Year > 2000 && date.Year < 2100;
+
+            return isValid;
+        }
+
+        public void SetStartAndEndYear(int maxTryCount, out int startYear, out int endYear)
+        {
+            int currentTryCount;
+            currentTryCount = 0;
+            while (!EnterDate(maxTryCount, ++currentTryCount, "start", out startYear)) { }
+
+            currentTryCount = 0;
+            while (!EnterDate(maxTryCount, ++currentTryCount, "end", out endYear)) { }
+
+            if (startYear > endYear)
+            {
+                int tempYear = startYear;
+                startYear = endYear;
+                endYear = tempYear;
+            }
+        }
+
+        public bool EnterDate(int maxTryCount, int currentTryCount, string yearName, out int year)
+        {
+            year = 0;
+            ConsoleHelper.WriteInfo($"Enter the {yearName} year (between 2000 and 2100):");
+            if (currentTryCount > 1)
+                ConsoleHelper.WriteVerbose($"Try {currentTryCount} / {maxTryCount}");
+            string? enteredYear = Console.ReadLine();
+
+            ConsoleHelper.ClearLastLines();
+
+            bool valid = !string.IsNullOrEmpty(enteredYear) && ValidateYear(enteredYear, out year);
+
+            if (!valid)
+            {
+                ConsoleHelper.WriteWarning($"The entered year ('{enteredYear}') is not valid.");
+                if (currentTryCount >= maxTryCount)
+                    throw new ArgumentException("The entered year is not in the right format. Integer between 2000 and 2100.");
+            }
+            else
+            {
+                ConsoleHelper.WriteSuccess($"The {yearName} year is '{enteredYear}'.");
+            }
+
+            return valid;
+        }
+
+        public bool ValidateYear(string yearValue, out int year)
+        {
+            bool isValid = int.TryParse(yearValue, out year);
+
+            isValid = isValid && year > 2000 && year < 2100;
+
+            return isValid;
+        }
+
+
+    }
+}
---
+++ DataGenerator/Program.cs
@@ -0,0 +1,62 @@
+﻿using DataGenerator.Domain;
+using Puffix.ConsoleLogMagnifier;
+
+const int maxTryCount = 5;
+
+ConsoleHelper.Write("Welcome to the data generator console App.");
+ConsoleHelper.Write("This help will generate sample data for your sample dashboards.");
+
+GeneratorService service = GeneratorService.CreateNew();
+
+ConsoleKey key;
+
+do
+{
+    ConsoleHelper.WriteInfo("Press Y to specify start and end year for data generation, D to specify start and end date for data generation, to Q to quit.");
+    key = Console.ReadKey().Key;
+    ConsoleHelper.ClearLastCharacters(1);
+    try
+    {
+        if (key == ConsoleKey.Q)
+        {
+            ConsoleHelper.WriteInfo("Thank you for using the data generator console App. See you soon!");
+        }
+        else if (key == ConsoleKey.Y)
+        {
+            service.SetStartAndEndYear(maxTryCount, out int startYear, out int endYear);
+            ConsoleHelper.WriteInfo($"Generate data for the period between {startYear} and {endYear}");
+
+            DateOnly startDate = new DateOnly(startYear, 1, 1);
+            DateOnly endDate = new DateOnly(endYear, 12, 31);
+
+            // TODO generate data
+            // TODO save data
+            // TODO manage holidays
+
+            ConsoleHelper.WriteWarning("Under construction");
+        }
+        else if (key == ConsoleKey.D)
+        {
+            service.SetStartAndEndDate(maxTryCount, out DateOnly startDate, out DateOnly endDate);
+            ConsoleHelper.WriteInfo($"Generate data for the period between {startDate} and {endDate}");
+
+            // TODO generate data
+            // TODO save data
+            // TODO manage holidays
+
+            ConsoleHelper.WriteWarning("Under construction");
+
+        }
+        else
+        {
+            ConsoleHelper.WriteWarning($"The key {key} is not a known command (for the moment :-) )");
+        }
+    }
+    catch (Exception error)
+    {
+        ConsoleHelper.WriteError("Error while generating data");
+        ConsoleHelper.WriteError(error);
+        ConsoleHelper.WriteNewLine(2);
+    }
+} while (key != ConsoleKey.Q);
+
